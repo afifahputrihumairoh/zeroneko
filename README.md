24a82e4c512d3f7db8873cdf61c9635ba1b5e22a
afifahputrihumairoh <108620897+afifahputrihumairoh@users.noreply.github.com>
2022-10-24 01:08:35 +0700

    Challenge IDCamp 2022
    Challenge IDCamp 2022

---
+++ README.md
@@ -0,0 +1,4 @@
+Cara Menjalankan
+1. Ketik Diterminal npm install
+2. lalu tunggu proses install selesai lalu ketik npm run start
+3. lalu buka browser masukkan http://localhost:3000/
---
+++ src/data/me.js
@@ -0,0 +1,17 @@
+export const me = [
+    {
+        path: "https://www.github.com/",
+        title: "Github",
+        icon: ""
+    },
+    {
+        path: "https://www.twitter.com/",
+        title: "Twitter",
+        icon: ""
+    },
+    {
+        path: "https://www.instagram.com/",
+        title: "Instagram",
+        icon: ""
+    },
+]
